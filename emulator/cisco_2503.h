a1a8af6d15d7df7a4879601ea0984b802384f764
Charles Burgoyne <charles.burgoyne@ents24.com>
2020-11-14 02:15:28 +0000

    Emulator: Defined peripheral address/size from uClinux memory map.

--- emulator/cisco_2503.h
+++ emulator/cisco_2503.h
@@ -2,36 +2,61 @@
 #define C2503__HEADER
 
 // Cisco hardware defines
-#define	C2503_CPU		M68K_CPU_TYPE_68EC030
+#define	C2503_CPU			M68K_CPU_TYPE_68EC030
 
 // Boot ROM (PLCC32 FW1/FW2)
-#define C2503_BOOTROM_SIZE	0x200000			// 2MB Boot ROM
-#define C2503_BOOTROM_ADDR1	0x00000000			// Starting location of boot ROM
-#define C2503_BOOTROM_ADDR2	0x01000000			// Alternate location of boot ROM
+#define C2503_BOOTROM_SIZE		0x200000			// 2MB Boot ROM
+#define C2503_BOOTROM_ADDR1		0x00000000			// Start address of boot ROM
+#define C2503_BOOTROM_ADDR2		0x01000000			// Alternate address of boot ROM
 
-#define C2503_RAM_SIZE		0x100				// 256 bytes of RAM
-#define C2503_RAM_ADDR1		0x00000000			// Starting location of RAM
+// RAM
+#define C2503_RAM_SIZE			0x100				// 256 bytes of RAM
+#define C2503_RAM_ADDR1			0x00000000			// Start address of RAM
 
-/* Old Memory-mapped IO ports */
-#define INPUT_ADDRESS		0x800000
-#define OUTPUT_ADDRESS		0x400000
+// I/O
+#define C2503_IO_ADDR			0x02100000			// I/O start address
+#define C2503_IO_SIZE			0x40000				// 256k window for I/O
+#define C2503_IO_68302_REG_ADDR		0x02100000			// 68302 registers address
+#define C2503_IO_68302_REG_SIZE		0x1000				// 68302 registers window size
+#define C2503_IO_68302_RAM_ADDR		0x02101000			// 68302 RAM address
+#define C2503_IO_68302_RAM_SIZE		0x1000				// 68302 RAM window size
+#define C2503_IO_SYS_CONTROL1_ADDR	0x02110000			// System control register 1 address
+#define C2503_IO_SYS_CONTROL1_SIZE	0x1				// System control register 1 size
+#define C2503_IO_SYS_CONTROL2_ADDR	0x02110002			// System control register 1 address
+#define C2503_IO_SYS_CONTROL2_SIZE	0x1				// System control register 1 size
+#define C2503_IO_SYS_STATUS_ADDR	0x02110100			// System status register address
+#define C2503_IO_SYS_STATUS_SIZE	0x1				// System status register size
+#define C2503_IO_SYS_ID_COOKIE_ADDR	0x02110040			// System ID cookie address
+#define C2503_IO_SYS_ID_COOKIE_SIZE	0x20				// System ID cookie size
+#define C2503_IO_COUNTER_TIMER_ADDR	0x02120000			// Counter/Timer regiter address
+#define C2503_IO_COUNTER_TIMER_SIZE	0x4				// Counter/Timer register size
+#define C2503_IO_COUNTER_CONTROL_ADDR	0x02120040			// Counter/Timer control register address
+#define C2503_IO_COUNTER_CONTROL_SIZE	0x1				// Counter/Timer control register size
+#define C2503_IO_DUART_ADDR		0x02120100			// DUART address
+#define C2503_IO_DUART_ADDR		0x40				// DUART size
+#define C2503_IO_CHANNELA_LANCE_ADDR	0x02130000			// Channel A: LANCE address
+#define C2503_IO_CHANNELA_LANCE_SIZE	0x4				// Channel A: LANCE size
+#define C2503_IO_CHANNELB_LANCE_ADDR	0x02130040			// Channel B: LANCE/serial address
+#define C2503_IO_CHANNELB_LANCE_SIZE	0x4				// Channel B: LANCE/serial size
+#define C2503_IO_CHANNELB_SERIAL_ADDR	0x02130080			// Channel B: serial DTR address
+#define C2503_IO_CHANNELB_SERIAL_SIZE	0x4				// Channel B: serial DTR size
 
 /* IRQ connections */
-#define IRQ_NMI_DEVICE		7
-#define IRQ_INPUT_DEVICE	2
-#define IRQ_OUTPUT_DEVICE	1
+#define IRQ_NMI_DEVICE			7
+#define IRQ_INPUT_DEVICE		2
+#define IRQ_OUTPUT_DEVICE		1
 
 /* Time between characters sent to output device (seconds) */
-#define OUTPUT_DEVICE_PERIOD	1
+#define OUTPUT_DEVICE_PERIOD		1
 
 // NCurses definitions
-#define EMU_WIN_CODE_COLS_MAX	75				// Maximum number of columns to allocate to the code window
-#define EMU_WIN_REG_ROWS_MAX	7				// Maximum number of rows to allocate to the registers window
-#define EMU_WIN_STATUS_ROWS_MAX	1				// Maximum number of rows to allocate to the status window
+#define EMU_WIN_CODE_COLS_MAX		75				// Maximum number of columns to allocate to the code window
+#define EMU_WIN_REG_ROWS_MAX		7				// Maximum number of rows to allocate to the registers window
+#define EMU_WIN_STATUS_ROWS_MAX		1				// Maximum number of rows to allocate to the status window
 
 // Disassembler
-#define cpu_read_word_dasm	mem_pgrm_read_word
-#define cpu_read_long_dasm	mem_pgrm_read_long
+#define cpu_read_word_dasm		mem_pgrm_read_word
+#define cpu_read_long_dasm		mem_pgrm_read_long
 
 // Macros
 //////////////////////////////////////////////////////////////////////////////////////////////
