8dcd4b634abe90c49aaf2ffee84b6eb076d975c8
Charles Burgoyne <charles.burgoyne@ents24.com>
2020-12-18 16:48:16 +0000

    Monitor: cisco2550, defined memory addresses for monitor variables.

--- monitor/cisco-2500/cisco-2500.h
+++ monitor/cisco-2500/cisco-2500.h
@@ -13,5 +13,20 @@
 .equ		EXCEPT_RESET_PC, bootrom_start + monitor_start
 
 # Define the boundaries for searching for modules
-.equ		module_search_mem_start, monitor_start
+#.equ		module_search_mem_start, monitor_start
+.equ		module_search_mem_start, bootrom_start
 .equ		module_search_mem_end, bootrom_start + bootrom_size
+
+# Monitor variable storage (0x500-0x6FF)
+.equ		MONITOR_ADDR_TEMP0, 0x00000500			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_TEMP1, 0x00000504			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_TEMP2, 0x00000508			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_TEMP3, 0x0000050c			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_TEMP4, 0x00000510			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_TEMP5, 0x00000514			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_TEMP6, 0x00000518			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_TEMP7, 0x0000051c			/* Memory address for temp storage */
+.equ		MONITOR_ADDR_CURRENT, 0x000005f8		/* Memory address for storing the current address */
+.equ		MONITOR_ADDR_ATTRIBUTES1, 0x000005fc		/* Memory address for monitor config */
+.equ		MONITOR_ADDR_STR_BUFFER, 0x00000600		/* Pointer to buffer */
+.equ		MONITOR_ADDR_STR_BUFFER_SIZE, 0x100		/* Buffer length */
