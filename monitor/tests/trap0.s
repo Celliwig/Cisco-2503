6cb8326229935f07ab0ba55c36a8f8b3e5ca1efb
Charles Burgoyne <charles.burgoyne@ents24.com>
2020-12-27 18:35:00 +0000

    Monitor: Added TRAP0 test.

---
+++ monitor/tests/trap0.s
@@ -0,0 +1,20 @@
+###########################################################################
+#                                                                         #
+#              Test exception handling by executing TRAP0                 #
+#                                                                         #
+###########################################################################
+
+.org	0x0
+	dc.b	0xA5,0xE5,0xE0,0xA5                                     /* signiture bytes */
+	dc.b	254,'!',0,0                                             /* id (254=cmd) */
+	dc.b	0,0,0,0                                                 /* prompt code vector */
+	dc.b	0,0,0,0                                                 /* reserved */
+	dc.b	0,0,0,0                                                 /* reserved */
+	dc.b	0,0,0,0                                                 /* reserved */
+	dc.b	0,0,0,0                                                 /* user defined */
+	dc.b	255,255,255,255                                         /* length and checksum (255=unused) */
+	.asciz	"TRAP0"
+
+.org	0x40
+	trap	#0							/* Execute TRAP 0 */
+	rts
