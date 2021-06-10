df115f0777c2dc5c6cc11209a20f111b06fada13
Celliwig <celliwig@nym.hush.com>
2021-06-10 15:31:03 +0100

    U-boot: Include M680x0 external interrupt definitions.

--- u-boot/arch/m68k/include/asm/immap.h
+++ u-boot/arch/m68k/include/asm/immap.h
@@ -460,8 +460,9 @@
 #endif				/* CONFIG_M548x */
 
 #ifdef CONFIG_M68030
+#include <asm/m680x0.h>
+
 #define CONFIG_SYS_NUM_IRQS             (192)
 #endif                          /* CONFIG_M68030 */
-
 
 #endif				/* __IMMAP_H */
